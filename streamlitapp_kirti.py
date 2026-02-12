6c4cee1b84799f62a9bd79477a7b77b575562e2b
kirti <kirti.sri1987@gmail.com>
2026-02-12 11:22:47 +0530

    streamlit new

--- streamlitapp_kirti.py
+++ streamlitapp_kirti.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1FuPbiOuK2J7eUi-67Kp9zybxE4rCxWvB
 """
 
-pip install streamlit pandas numpy joblib scikit-learn matplotlib seaborn
-
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile app.py
 # import os;
